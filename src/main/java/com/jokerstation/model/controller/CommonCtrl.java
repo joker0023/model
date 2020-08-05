26b692b69857ec0abb6f31c9a372fd551cc101dc
weijie.li <weijie.li@wehotelglobal.com>
2020-08-05 16:17:34 +0800

    配置优化

--- src/main/java/com/jokerstation/model/controller/CommonCtrl.java
+++ src/main/java/com/jokerstation/model/controller/CommonCtrl.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class CommonCtrl {
 	
@@ -14,7 +15,7 @@ public class CommonCtrl {
 	public String test() {
 		logger.info("test in ...");
 //		logger.debug("debug log test");
-//		logger.info("info log test");
+//		loggenir.info("info log test");
 //		logger.warn("warn log test");
 //		logger.error("error log test");
 		return "success";
